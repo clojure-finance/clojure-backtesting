5690c04a749b19922d759b77ded02addfae58f1d
Joey Pathak-Dubey <joeydubey@gmail.com>
2020-09-09 15:31:29 +0800

    Added function to import CSVs

--- project.clj
+++ project.clj
@@ -3,5 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
-  :repl-options {:init-ns clojure-backtesting.core})
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/data.csv "1.0.0"]]
+  :main ^:skip-aot clojure-backtesting.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
--- src/clojure_backtesting/core.clj
+++ src/clojure_backtesting/core.clj
@@ -1,6 +1,18 @@
-(ns clojure-backtesting.core)
+(ns clojure-backtesting.core
+  (:gen-class)
+  (:require [clojure.data.csv :as csv] ;; Useful for CSV handling
+            [clojure.java.io :as io])) ;; For input-output handling
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn load-csv
+  "Loads a specified CSV file from the resources folder
+  in project directory"
+  [filename]
+  (with-open [file (io/reader (str "resources/" filename))]
+    (doall
+     (csv/read-csv file))))
+
+(defn get-keys
+  "Converts the first row in a vector of vectors to
+  mappable keywords:"
+  [head & tail]
+  (map keyword head))
