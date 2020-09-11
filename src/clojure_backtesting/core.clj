7cfb64380040a271fffcad309fc224e1123172f9
Yuchen Liu <43634213+hkulyc@users.noreply.github.com>
2020-09-12 01:09:39 +0800

    can read the csv as map

--- src/clojure_backtesting/core.clj
+++ src/clojure_backtesting/core.clj
@@ -1,7 +1,9 @@
 (ns clojure-backtesting.core
   (:gen-class)
   (:require [clojure.data.csv :as csv] ;; Useful for CSV handling
-            [clojure.java.io :as io])) ;; For input-output handling
+            [clojure.java.io :as io]
+            [clojure.set :as set]
+            [clojure.pprint :as pprint])) ;; For input-output handling
 
 (defn load-csv
   "Loads a specified CSV file from the resources folder
@@ -16,3 +18,32 @@
   mappable keywords:"
   [head & tail]
   (map keyword head))
+
+(defn test [] println "x")(defn csv->map
+  "Convert parsed CSV vectors into maps with headers as keys"
+  [csv-data]
+  (map zipmap
+       (->> (first csv-data)
+            (map keyword)
+            repeat)
+       (rest csv-data)))
+
+(defn update-by-keys
+  "Update values in a map (m) by applying function (f) on keys"
+  [m keys f]
+  (reduce (fn [m k] (update m k f)) m keys))
+
+(defn parse-int
+  "Parse integer from string. May return nil."
+  [str]
+  (try (Integer/parseInt str)
+       (catch Exception e nil)))
+
+(defn slurp-csv
+  "Read CSV data into memory"
+  [filename]
+  (with-open [reader (io/reader filename)]
+    (->> (csv/read-csv reader)
+         csv->map doall)))
+
+
