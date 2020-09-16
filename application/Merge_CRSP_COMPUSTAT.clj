7be94098b365d952715ccb1537dcfa4a7f0a3378
Yuchen Liu <43634213+hkulyc@users.noreply.github.com>
2020-09-16 21:39:02 +0800

    solve some bugs in the function sequance

--- application/Merge_CRSP_COMPUSTAT.clj
+++ application/Merge_CRSP_COMPUSTAT.clj
@@ -19,6 +19,29 @@
   )
 )
 
+(defn last-quar
+  "return the last quarter date of a given row"
+  [row]
+  (let [date (get row :date)]
+    (let [[year month day] (map parse-int (str/split date #"/"))]
+      (cond
+      (or (and (= month 12) (= day 31))(= month 1) (= month 2) (and (= month 3) (<= day 30))) (str (- year 1) "/" 12 "/" 31)
+      (or (and (= month 3) (= day 31)) (= month 4) (= month 5) (and (= month 6) (<= day 29))) (str year "/3/" 31)
+      (or (and (= month 6) (= day 30)) (= month 7) (= month 8) (and (= month 9) (<= day 30))) (str year "/6/" 30)
+      (or (and (= month 9) (= day 31)) (= month 10) (= month 11) (and (= month 12) (<= day 30))) (str year "/" 9 "/" 31))
+    )
+  )
+)
+
+
+(defn insert-col
+  "insert the date col from COMPUSTAT into CRSP"
+  [file1 set] ;; key: before the insert col; file1: insert into this file; set: col to be inserted
+  ;;add a thing in each map using key and value
+  ;;run loop, length of the loop = length of the list
+
+  (for [row file1] (concat row {:datadate (condp contains? (last-quar row) set (last-quar row) "")}))
+)
 
 (defn merge-data
   "merge 2 csv files "
@@ -31,7 +54,7 @@
    (def date-set (set (get-set (file2 :datadate))))
 
    (def mergefile 
-     (insert-col f1 f2 date-set )
+     (insert-col f1 date-set )
 
 
    )
@@ -42,28 +65,4 @@
   
   ;; need to parse-int later
 )
-
-(defn insert-col
-  "insert the date col from COMPUSTAT into CRSP"
-  [file1 set] ;; key: before the insert col; file1: insert into this file; set: col to be inserted
-  ;;add a thing in each map using key and value
-  ;;run loop, length of the loop = length of the list
-
-  (for [row file1] (concat row {:datadate (condp contains? (last-quar row) set (last-quar row) "")}))
-)
-
-  (defn last-quar
-    "return the last quarter date of a given row"
-    [row]
-    (let [date (get row :date)]
-      (let [[year month day] (map parse-int (str/split date #"/"))]
-        (cond
-        (or (and (= month 12) (= day 31))(= month 1) (= month 2) (and (= month 3) (<= day 30))) (str (- year 1) "/" 12 "/" 31)
-        (or (and (= month 3) (= day 31)) (= month 4) (= month 5) (and (= month 6) (<= day 29))) (str year "/3/" 31)
-        (or (and (= month 6) (= day 30)) (= month 7) (= month 8) (and (= month 9) (<= day 30))) (str year "/6/" 30)
-        (or (and (= month 9) (= day 31)) (= month 10) (= month 11) (and (= month 12) (<= day 30))) (str year "/" 9 "/" 31))
-      )
-    )
-  )
-
 
