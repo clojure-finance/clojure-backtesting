d766d9088d04c0500e9e13f94e15831a5d82c510
Kony Kwong <konykwong@yahoo.com.hk>
2020-09-16 20:48:58 +0800

    Update Merge_CRSP_COMPUSTAT.clj

--- application/Merge_CRSP_COMPUSTAT.clj
+++ application/Merge_CRSP_COMPUSTAT.clj
@@ -23,11 +23,18 @@
 (defn merge-data
   "merge 2 csv files "
   [file1 file2]
-  ((def file1 (slurp-csv file1)) ;;file 1 is CRSP
+  (
+   (def f1 (slurp-csv file1)) ;;file 1 is CRSP
   
-  (def file2 (slurp-csv file2)) ;;file 2 Is COMPUSTAT
-  
-  () 
+   (def f2 (slurp-csv file2)) ;;file 2 Is COMPUSTAT
+   
+   (def date-set (set (get-set (file2 :datadate))))
+
+   (def mergefile 
+     (insert-col f1 f2 date-set )
+
+
+   )
 
   ;;(def file0 (insert-col file1 set))
    
@@ -40,8 +47,8 @@
   "insert the date col from COMPUSTAT into CRSP"
   [file1 set] ;; key: before the insert col; file1: insert into this file; set: col to be inserted
   ;;add a thing in each map using key and value
-  ;;run loop, length of the loop = length of the list, 
-  ;;read the value of the key "date", compare with "leo think of the algo"
+  ;;run loop, length of the loop = length of the list
+
   (for [row file1] (concat row {:datadate (condp contains? (last-quar row) set (last-quar row) "")}))
 )
 
