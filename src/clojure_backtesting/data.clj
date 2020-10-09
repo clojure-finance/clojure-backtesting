6037b2f97d9bed825510db5383f80c339e540082
Angel Woo <awoo424@gmail.com>
2020-10-09 21:18:30 +0800

    Add comments

--- src/clojure_backtesting/data.clj
+++ src/clojure_backtesting/data.clj
@@ -9,11 +9,8 @@
 
 (def data-set (atom [])) ;; main dataset (to be changed by the user)
 
-; (defn test
-;   (def f0 1))
-
 (defn csv->map
-  "Convert parsed CSV vectors into maps with headers as keys"
+  "Convert parsed CSV vectors into maps with headers as keys, by row"
   [csv-data]
   (map zipmap ;; make the first row as headers and the following rows as values in a map structure e.g. {:tic AAPL} 
        (->> (first csv-data) ;; take the first row of the csv-data
@@ -22,6 +19,7 @@
        (rest csv-data))) ;; use the rest rows as values of the map
 
 (defn csv->map_col
+  "Convert parsed CSV vectors into maps with headers as keys, by column"
   [csv-data]
   (zipmap
     (->> (first csv-data)
@@ -29,9 +27,9 @@
     (apply map vector (rest csv-data))))
 
 (defn update-by-keys
-  "Update values in a map (m) by applying function (f) on keys"
-  [m keys f]
-  (reduce (fn [m k] (update m k f)) m keys))  
+  "Update values in a map by applying function (f) on keys"
+  [map keys f]
+  (reduce (fn [map k] (update map k f)) map keys))  
 
 (defn parse-int
   "Parse integer from string. May return nil."
@@ -39,27 +37,29 @@
   (try (Integer/parseInt str) 
        (catch Exception e nil)))
 
+; Returns a list of maps, e.g. ({:col1 1, :col2 2} {:col1 3, :col2 4})
 (defn read-csv-row
-  "Read CSV data into memory"
+  "Read CSV data into memory by row"
   [filename]
   (with-open [reader (io/reader filename)]
     (->> (csv/read-csv reader)
-         csv->map           ;; change the csv to a map with the csv->map fn
+         csv->map
          doall)))
 
+; Returns a map, e.g. {:col1 [1 3], :col2 [2 4]}
 (defn read-csv-col
-  "Read CSV data into memory"
+  "Read CSV data into memory by column"
   [filename]
   (with-open [reader (io/reader filename)]
     (->> (csv/read-csv reader)
-         csv->map_col          ;; change the csv to a map with the csv->map fn
+         csv->map_col
          doall)))
 
-;;Here, I want to define a function that can convert between the column based
-;;dataset and the row based data set
+;; Here, I want to define a function that can convert between the column based
+;; dataset and the row based dataset
 (defn row->col
   [row-based]
-  "This function can parse the seq like ({}{}{}) to {: [] : []}"
+  "This function can parse the seq like ({} {} {}) to {: [] : []}"
     (-> (keys (first row-based))
     (zipmap (apply map vector (map vals (rest row-based))))))
 
@@ -87,10 +87,8 @@
                  (conj ret x)))
              #{} xrel))))
 
-
-;;file 1 and 2 address store for testing purpose
-(def file1 "./resources/CRSP-extract_test.csv")
-(def file2 "./resources/Compustat-extract_test.csv")
+; (def file1 "./resources/CRSP-extract_test.csv")
+; (def file2 "./resources/Compustat-extract_test.csv")
 
 ;;file 1 and 2 directories for Kony
 ;;(def file1 "/home/kony/Documents/GitHub/clojure-backtesting/resources/CRSP-extract.csv")
@@ -98,6 +96,3 @@
 
 ;;(def a (read-csv-row file1))
 ;;(def b (read-csv-row file2))
-
-(def memory (read-csv-col file1))
-(println (count (take 1 memory)))
