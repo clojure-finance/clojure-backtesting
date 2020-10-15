12187bf6f8d20e2a4dc8d71cd3733377dc0b363d
DavidJTMa <64685281+DavidJTMa@users.noreply.github.com>
2020-10-16 00:42:50 +0800

    add the update_portfolio function

--- src/clojure_backtesting/order.clj
+++ src/clojure_backtesting/order.clj
@@ -8,20 +8,27 @@
 (def total_record (atom{}))
 ;; Create initial portfolio with cash only (User input thei initial-capital)
 (defn initiate_portfolio
-  [init-capital]
-  (def portfolio (atom {:cash init-capital}))
-  (def portfolio_value (atom init-capital)))
+
+  [date init-capital]
+
+  (def portfolio (atom {:cash {:tot_val init-capital}}))
+  (def portfolio_value (atom [{:date date :tot_value init-capital}])))
 
 (defn update_portfolio
-  [tic quantity price]
+
+  [date tic quantity price]
+
   (if-not (contains? (deref portfolio) tic)
     (let [tot_val (* price quantity)]
       (do (swap! portfolio (fn [curr_port] (conj curr_port [tic {:price price :quantity quantity :tot_val tot_val}])))
-      (swap! portfolio assoc :cash (- (:cash (deref portfolio)) tot_val))))
+      (swap! portfolio assoc :cash {:tot_val (- (get-in (deref portfolio) [:cash :tot_val]) tot_val)})))
+
     (let [[tot_val qty] [(* price quantity) (get-in (deref portfolio) [tic :quantity])]]
-      (do (swap! portfolio assoc tic [:price price :quantity (+ qty quantity) :tot_val (* price (+ qty quantity))])
-      (swap! portfolio assoc :cash (- (:cash (deref portfolio)) tot_val)))))
-      )
+      (do (swap! portfolio assoc tic {:price price :quantity (+ qty quantity) :tot_val (* price (+ qty quantity))})
+      (swap! portfolio assoc :cash {:tot_val (- (get-in (deref portfolio) [:cash :tot_val]) tot_val)}))))
+
+    (let [tot_value (reduce + (map :tot_val (vals (deref portfolio))))]
+      (swap! portfolio_value (fn [curr_port_val] (conj curr_port_val {:date date :tot_value tot_value})))))
 
 (defn search_in_order
 	"This function try to retrieve the matching entry from the dataset"
