a3d1671f979c054efaa251dafd875e759e2e8f98
Angel Woo <awoo424@gmail.com>
2021-01-10 23:12:38 +0800

    Plotting function overloading

--- src/clojure_backtesting/plot.clj
+++ src/clojure_backtesting/plot.clj
@@ -81,6 +81,76 @@
   ))
 )
 
+;; Plotting function
+(defn plot
+  "this is the function that allows the users to plot charts,"
+  ([dataset series x y full-date]
+    (if (= true full-date)
+      (oz/view! ; with full-date
+        { :width 800 :height 500 ;adjust the graph size
+          :data      {:values dataset}
+          :encoding  {:x {:field x :type "temporal" :timeUnit "yearmonthdate"};"field" means the x-axis name, "type" asking what's the data type of x-axis values; choosing from "quantity"/"nominal"/"temporal"  
+                      :y {:field y :type "quantitative"}
+                      :color {:field series :type "nominal"} ;if it's only 1 line, no need this
+                    } 
+          :mark "line"
+        }
+      )
+      (oz/view! ; w/o full date
+        { :width 800 :height 500 ;adjust the graph size
+          :data      {:values dataset}
+          :encoding  {:x {:field x :type "temporal"};"field" means the x-axis name, "type" asking what's the data type of x-axis values; choosing from "quantity"/"nominal"/"temporal"  
+                      :y {:field y :type "quantitative"}
+                      :color {:field series :type "nominal"} ;if it's only 1 line, no need this
+                    } 
+          :mark "line"
+        }
+      )
+    )
+  )
+  
+  ([dataset series x y1 y2 full-date] ;y1 & y2 should be key for values to be plotted, e.g. :tot-value or :daily-ret
+    (if (= true full-date)
+      (oz/view!
+        { :width 800 :height 500 ;adjust the graph size
+          :data      {:values dataset}
+          :encoding  {:x {:field x :type "temporal" :timeUnit "yearmonthdate"} ;"field" means the x-axis name, "type" asking what's the data type of x-axis values; choosing from "quantity"/"nominal"/"temporal"  
+                      :color {:field series :type "nominal"} ;if it's only 1 line, no need this
+                    } 
+          :layer     [
+                    {:mark {:type "line"}
+                      :encoding {:y {:field y1 :type "quantitative"}
+                                :color {:field series :type "nominal"}}
+                    } 
+                    {:mark {:type "line"}
+                    :encoding {:y {:field y2 :type "quantitative"}
+                                :color {:field series :type "nominal"}}
+                    }]
+          :resolve   {:scale {:y "independent"}}
+        }
+      )
+      (oz/view!
+        { :width 800 :height 500 ;adjust the graph size
+          :data      {:values dataset}
+          :encoding  {:x {:field x :type "temporal"} ;"field" means the x-axis name, "type" asking what's the data type of x-axis values; choosing from "quantity"/"nominal"/"temporal"  
+                      :color {:field series :type "nominal"} ;if it's only 1 line, no need this
+                    } 
+          :layer     [
+                    {:mark {:type "line"}
+                      :encoding {:y {:field y1 :type "quantitative"}
+                                :color {:field series :type "nominal"}}
+                    } 
+                    {:mark {:type "line"}
+                    :encoding {:y {:field y2 :type "quantitative"}
+                                :color {:field series :type "nominal"}}
+                    }]
+          :resolve   {:scale {:y "independent"}}
+        }
+      )
+    )
+  )
+)
+
 
 
 
