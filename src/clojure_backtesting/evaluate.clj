cb7eac018d3ed8dbd4509bb99e3eb4786e9e5478
Angel Woo <awoo424@gmail.com>
2021-01-23 02:58:05 +0800

    Optimise evaluate.clj

--- src/clojure_backtesting/evaluate.clj
+++ src/clojure_backtesting/evaluate.clj
@@ -15,49 +15,10 @@
 ;; Calculate portfolio total returns
 (defn portfolio-total-ret
   "This function returns the current daily return of the portfolio in %."
-  []
-  ; (def total-ret 0.0)
-  ; (doseq [daily-record (deref portfolio-value)]
-  ;   (let [daily-ret (daily-record :daily-ret)]
-  ;     ;(println daily-ret)
-  ;     (def total-ret (+ total-ret daily-ret))
-  ;   )
-  ; )
-  ; total-ret
-  
-  ; new version
+  []  
   (get (last (deref portfolio-value)) :tot-ret)
 )
 
-;; Calculate number of days between first and last dates in order record
-(defn num-of-tradays
-  "This function calculates the annualised return of the portfolio." 
-  []
-  (let [first-date (get (first (deref order-record)) :date)
-          last-date (get (last (deref order-record)) :date)]
-
-    (def first-year (Integer/parseInt (subs first-date 0 4)))
-    (def first-month (Integer/parseInt (subs first-date 5 7)))
-    (def first-day (Integer/parseInt (subs first-date 8 10)))
-    (def last-year (Integer/parseInt (subs last-date 0 4)))
-    (def last-month (Integer/parseInt (subs last-date 5 7)))
-    (def last-day (Integer/parseInt (subs last-date 8 10)))
-
-    (def interval-min (clj-t/in-minutes (clj-t/interval (clj-t/date-time first-year first-month first-day) (clj-t/date-time last-year last-month last-day))))
-    (/ (/ interval-min 60) 24)
-  )
-)
-
-;; Return calculated with rolling fixed window of 1 year (in %)
-(defn rolling-return
-  "This function calculates the return of the portfolio, with a rolling 
-  fixed window of 1 year." 
-  []
-  (def tradays (num-of-tradays))
-  (if (= tradays 0) (def tradays 1))
-  ;(println tradays)
-  (- (Math/pow (+ 1 (/ (portfolio-total-ret) 100)) (/ 252 tradays)) 1)
-)
 
 ;; Helper function
 (defn square
@@ -117,25 +78,6 @@
   )
 )
 
-;; Volatility, rolling window (in %)
-(defn rolling-volatility
-  "This function returns the volatility of the portfolio in %, calculated with a
-  rolling fixed window of 1 year.."
-  []
-  (* (Math/sqrt 252) (standard-deviation (deref (get-daily-returns))))
-) 
-
-;; Sharpe ratio, rolling window (in %)
-(defn rolling-sharpe-ratio
-  "This function returns the sharpe ratio in %, calculated with a
-  rolling fixed window of 1 year."
-  []
-  (if (not= (rolling-volatility) 0.0)
-    (/ (rolling-return) (rolling-volatility))
-    0.0
-  )
-)
-
 ;; PnL per trade (in $)
 (defn pnl-per-trade
   "This function returns the profit/loss per trade in dollars."
@@ -149,39 +91,24 @@
   [date]
   (if (not= (count (deref order-record)) 0) ; check that order record is not empty
     (let [total-val-data (portfolio-total)
-        ;daily-ret-data (portfolio-daily-ret)
-        ;total-ret-data (portfolio-total-ret)
         volatility-data (volatility)
         sharpe-ratio-data (sharpe-ratio)
-        ;rolling-ret-data (rolling-return)
-        rolling-vol-data (rolling-volatility)
-        rolling-sharpe-data (rolling-sharpe-ratio)
         pnl-per-trade-data (pnl-per-trade)
         ]
       (do
         ; numerical values
         (swap! eval-record concat [(into (sorted-map) {:date date
           :tot-val total-val-data
-          ;:ret-da daily-ret-data 
-          ;:ret-tot total-ret-data
           :vol-e volatility-data
           :sharpe-e sharpe-ratio-data
-          ;:ret-r rolling-ret-data
-          :vol-r rolling-vol-data
-          :sharpe-r rolling-sharpe-data
           :pnl-pt pnl-per-trade-data
         })])
         
         ; string formatting
         (swap! eval-report-data concat [(into (sorted-map) {:date date
                 :tot-val (str "$" (int total-val-data))
-                ;:ret-da (str (format "%.2f" daily-ret-data) "%")
-                ;:ret-tot (str (format "%.2f" (* total-ret-data 100)) "%")
                 :vol-e (str (format "%.2f" volatility-data) "%")
                 :sharpe-e (str (format "%.2f" sharpe-ratio-data) "%")
-                ;:ret-r (str (format "%.2f" rolling-ret-data) "%")
-                :vol-r (str (format "%.2f" rolling-vol-data) "%")
-                :sharpe-r (str (format "%.2f" rolling-sharpe-data) "%")
                 :pnl-pt (str "$" (int pnl-per-trade-data))
         })])
       )
--- src/clojure_backtesting/user.clj
+++ src/clojure_backtesting/user.clj
@@ -7,6 +7,7 @@
             [clojure-backtesting.plot :refer :all]
             [clojure-backtesting.specs :refer :all]
             [clojure-backtesting.counter :refer :all]
+            [clojure-backtesting.parameters :refer :all]
             ;;[clojure-backtesting.parameters :refer :all]
             [clojure.string :as str]
             [clojure.pprint :as pprint]
@@ -37,26 +38,34 @@
   "Write your code here"
     [& args] ; pass ./resources/CRSP-extract.csv as arg
     (reset! data-set (add-aprc (read-csv-row "./resources/CRSP-extract.csv")))
-    (init-portfolio "1980-12-15" 10000)
-    (time (do (def MA50-vec [])
-              (def MA200-vec [])
-              (while (not= (get-date) "1981-03-10")
-                (do
-        ;; write your trading strategy here
-                  (def tics (deref available-tics-)) ;20 ms
-                  (def MA50-vec (get-prev-n-days :PRC 50 "AAPL" MA50-vec (get (get tics "AAPL") :reference)))
-        ;(println (get-date))
-        ;(println MA50-vec)
-                  (def MA200-vec (get-prev-n-days :PRC 200 "AAPL" MA200-vec (get (get tics "AAPL") :reference)))
-        ;(println MA200-vec)
-                  (let [[MA50 MA200] [(moving-average :PRC MA50-vec) (moving-average :PRC MA200-vec)]]
-                    (if (> MA50 MA200)
-                      (order "AAPL" 1 :reference (get (get tics "AAPL") :reference) :print true) ;1ms
-                      (order "AAPL" 0 :remaining true :reference (get (get tics "AAPL") :reference) :print true))) ;1ms
-                  (update-eval-report (get-date))
-                  (next-date)))))
+    (init-portfolio "1980-12-15" 1000000)
+    (time (do (def MA50-vec-aapl [])
+          (def MA200-vec-aapl [])
+          (def MA50-vec-f [])
+          (def MA200-vec-f [])
+          (while (not= (get-date) "1981-12-15")
+            (do
+              ;; write your trading strategy here
+              (def tics (deref available-tics-)) ;20 ms
+              (def MA50-vec-aapl (get-prev-n-days :PRC 50 "AAPL" MA50-vec-aapl (get (get tics "AAPL"):reference)))
+              (def MA200-vec-aapl (get-prev-n-days :PRC 200 "AAPL" MA200-vec-aapl (get (get tics "AAPL") :reference)))
+              (def MA50-vec-f (get-prev-n-days :PRC 50 "F" MA50-vec-f (get (get tics "F"):reference)))
+              (def MA200-vec-f (get-prev-n-days :PRC 200 "F" MA200-vec-f (get (get tics "F") :reference)))
+              (let [[MA50 MA200] [(moving-average :PRC MA50-vec-aapl) (moving-average :PRC MA200-vec-aapl)]]
+                (if (> MA50 MA200)
+                  (order "AAPL" 1 :reference (get (get tics "AAPL") :reference) :print false) 
+                  (order "AAPL" 0 :remaining true :reference (get (get tics "AAPL") :reference))))
+              (let [[MA50 MA200] [(moving-average :PRC MA50-vec-f) (moving-average :PRC MA200-vec-f)]]
+                (if (> MA50 MA200)
+                  (order "F" 1 :reference (get (get tics "F") :reference) :print false) 
+                  (order "F" 0 :remaining true :reference (get (get tics "F") :reference))))
+              (next-date)))))
+    ;(update-eval-report (get-date))
     (.close wrtr)
+    (pprint/print-table (deref order-record))
     (view-portfolio-record)
+    
+    (update-eval-report (get-date))
     (eval-report)
  )
 
