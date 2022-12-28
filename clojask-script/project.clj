06580599e20028926119c8ec0418b0b9f2f3af40
Yuchen Liu <43634213+hkulyc@users.noreply.github.com>
2022-12-28 21:39:13 +0800

    script uses 2.0.1

--- clojask-script/project.clj
+++ clojask-script/project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.github.clojure-finance/clojask "2.0.0"]]
+                 [com.github.clojure-finance/clojask "2.0.1"]]
   :repl-options {:init-ns clojask-script.core
                  :timeout 18000}
   :main clojask-script.core/-main)
